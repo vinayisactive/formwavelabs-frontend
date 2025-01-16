e925378421514db84a9e6c9123e97d5d1b040586
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-16 23:50:26 +0530

    chore: initialize project with NextJs, Tailwind CSS and basic folder structure

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+    <p>
+        Sign-in
+    </p>
+</div>
+  )
+}
+
+export default SignIn
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+        <p>
+            Sign-up
+        </p>
+    </div>
+  )
+}
+
+export default SignUp
