07ae2061aef031d573488540e1f639ea0d5e68e8
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-17 01:10:37 +0530

    chor: add redirect for form-builder route and types file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ async redirects(){
+  return [
+    {
+      source: "/form/:formId",
+      destination: "/form/:formId/builder",
+      permanent: true
+    }
+  ]
+ }
 };
 
 export default nextConfig;
