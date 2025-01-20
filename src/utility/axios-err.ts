a48400902258b2d93d73bbcadbb62b3400a45e69
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-20 23:43:22 +0530

    feat: add axios-error handing utility

---
+++ src/utility/axios-err.ts
@@ -0,0 +1,19 @@
+import { AxiosError } from "axios";
+
+interface ErrorResponse {
+  message?: string; 
+}
+
+export const handleAxiosError = (error: unknown): string => {
+  if ((error as AxiosError).isAxiosError) {
+    const axiosError = error as AxiosError<ErrorResponse>
+
+    if (axiosError.response) {
+      return axiosError.response.data?.message|| "An error occurred. Please try again.";
+    } else if (axiosError.request) {
+      return "Network error. Please check your connection.";
+    }
+  }
+  
+  return "Something went wrong. Please try again.";
+};
