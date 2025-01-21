b4a12492654425bd4e7c2e1d78ade507cce7dad6
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-22 02:02:10 +0530

    chor: update next.config to handle redirects

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- async redirects(){
-  return [
-    {
-      source: "/form/:formId",
-      destination: "/form/:formId/builder",
-      permanent: true
-    }
-  ]
- }
+  async redirects(){
+    return [
+      {
+        source: "/form/:formId",
+        destination: "/form/:formId/builder",
+        permanent: true
+      }
+    ]
+  }
 };
 
 export default nextConfig;
