51494d7db621508d0d76bdbe1db9a2a2ce32d593
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-01-25 15:59:34 +0530

    feat: add element-context provider and handle params

--- src/app/submit/[formId]/page.tsx
+++ src/app/submit/[formId]/page.tsx
@@ -1,11 +1,19 @@
+import Submission from '@/components/section/submission';
+import ElementsContextProvider from '@/context/elements-context';
 import React from 'react'
 
-const page = () => {
+const page = async({params}: {params: Promise<{formId: string}>}) => {
+  const formId = (await params).formId; 
+
+  // console.log(`From id from submission : ${formId}`);
+
   return (
-    <div>
-      
-    </div>
+    <ElementsContextProvider>
+      <div className='w-screen h-screen pt-14'>
+      <Submission formId = {formId}/>
+      </div>
+    </ElementsContextProvider>
   )
 }
 
-export default page
+export default page; 
