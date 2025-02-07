4baa421fc36a32ea74a9c89790c1f6a8753f897d
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-02-08 00:22:58 +0530

    test: testing file/pdf upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
+import ImageKitFileUpload from '@/components/ui/imagekit-file-uploader'
 import React from 'react'
 
 const Page = () => {
   return (
     <div className=' h-full flex flex-col gap-2 justify-center items-center'>
-        <p className='text-sm'>
-            Formwavelabs
-        </p>
+       <ImageKitFileUpload fileType="pdf"/>
     </div>
   )
 }
