66d5895eb392c8bd105ebbf42548a2798decb15b
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-02-08 17:11:19 +0530

    refactor: remove test upload button from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-import ImageKitFileUpload from '@/components/ui/imagekit-file-uploader'
-import React from 'react'
+import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
+import React from "react";
 
 const Page = () => {
   return (
-    <div className=' h-full flex flex-col gap-2 justify-center items-center'>
-       <ImageKitFileUpload fileType="pdf"/>
+    <div className=" h-full flex flex-col gap-2 justify-center items-center">
+      <p>
+        you probably find the link from my github while stalking me, so please
+        you better follow me on twitter oki ?
+      </p>
+      <Link
+        href={"https://x.com/vinayisactive"}
+        target="_blank"
+        className="text-blue-400 flex justify-center items-center gap-2 hover:underline"
+      >
+        vinayisactive <ArrowUpRight className="text-sm" />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
