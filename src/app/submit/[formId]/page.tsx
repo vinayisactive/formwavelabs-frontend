32efbd97f94c8a0e1db27c883917121515a06fcf
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-02-09 17:36:42 +0530

    refactor: refactor submit page layout

--- src/app/submit/[formId]/page.tsx
+++ src/app/submit/[formId]/page.tsx
@@ -7,7 +7,7 @@ const page = async({params}: {params: Promise<{formId: string}>}) => {
 
   return (
     <ElementsContextProvider>
-      <div className='w-full bg-white absolute top-0 right-0 z-[100]'>
+      <div className='w-full bg-white absolute top-0 right-0 z-[100] flex justify-center '>
          <Submit formId = {formId}/>
       </div>
     </ElementsContextProvider>
