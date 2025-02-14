6819624f7d28e3f6d964e28f926117fa686911f1
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-02-14 22:59:26 +0530

    feat: add ManRepo font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/ui/navbar";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import LayoutProvider from "@/components/wrap/layout-provider-wrapper";
 
-const InterFont = Inter({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
+const manRope = Manrope({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Formwavelabs",
@@ -19,7 +15,7 @@ export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={InterFont.className}>
+    <html lang="en" className={manRope.className}>
       <body>
         <LayoutProvider>
           <div className="w-screen h-screen flex flex-col justify-center items-center">
