2041ee18349b3a84924e860c61f26cd08f69ea42
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-03-11 23:45:28 +0530

    feat: add real-time changes with react-query, workspace-navbar, workspace-members, and invite user components.

--- src/components/ui/workspace/workspace.tsx
+++ src/components/ui/workspace/workspace.tsx
@@ -1,68 +1,137 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FormCard from "../form-card";
-import { PanelLeft, PanelRight } from "lucide-react";
+import { useSession } from "next-auth/react";
+import WorkspaceNavbar from "./workspace-navbar";
+import WorkspaceSidebarMobile from "./workspace-sidebar-mobile";
+import { useQuery } from "@tanstack/react-query";
+import WorkSpaceInviteModal from "./workspace-invite-modal";
+import WorkspaceCreateForm from "./workspace-create-form";
 
+export interface MemberInterface {
+  role: string;
+  user: {
+    id: string;
+    name: string;
+  };
+}
 
 interface Form {
   id: string;
   title: string;
   status: boolean;
-  _count: { pages: number, submissions: number };
+  _count: { pages: number; submissions: number };
   createdAt: string;
 }
 
 interface WorkspaceData {
   forms: Form[];
-  members: [];
+  members: MemberInterface[];
   id: string;
+  name: string;
 }
 
-interface WorkspaceInterface {
+interface WorkspaceDataInterface {
   status: "success" | "error";
   data: WorkspaceData;
   message: string;
 }
 
+const Workspace = ({ wsId }: { wsId: string }) => {
+  const currentUserData = useSession().data;
+  const [workspaceData, setWorkspaceData] =
+    useState<WorkspaceDataInterface | null>(null);
 
-const Workspace = ({ workspace }: { workspace: WorkspaceInterface }) => {
-  const { forms, id } = workspace?.data;
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isInviteModalOpen, setInviteModalOpen] = useState<boolean>(false);
+  const [isCreateFormModal, setCreateFormModal] = useState<boolean>(false);
+  const [userRole, setRole] = useState<string | null>(null);
+
+  const { data } = useQuery({
+    queryKey: ["workspace", wsId],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://formwavelabs-backend.alfreed-ashwry.workers.dev/api/v1/workspaces/${wsId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${currentUserData?.accessToken}`,
+          },
+        }
+      );
+
+      if (!res.ok) throw new Error(res.statusText);
+      const data = await res.json();
+      setWorkspaceData(data);
+      return data;
+    },
+    enabled: Boolean(wsId),
+  });
+
+  useEffect(() => {
+    const currentUser = workspaceData?.data.members?.find(
+      (member) => member?.user?.id === currentUserData?.user.id
+    );
+
+    setRole(currentUser ? currentUser.role : null);
+  }, [workspaceData, data]);
 
   return (
-    <div className="w-full h-full overflow-auto bg-gray-100 px-2 md:px-6">
-
-      <div className="h-[6%] flex items-center">
-        <button onClick={() => setIsSidebarOpen(true)} className=" hover:bg-gray-100 rounded-lg md:hidden">
-          <PanelRight className="h-5 w-5" />
-        </button>
+    <div className="w-full h-full overflow-auto bg-gray-100 px-2 md:px-4 shadow-inner">
+      <div className="h-[6%] flex items-center gap-2">
+        <WorkspaceNavbar
+          workspaceName={workspaceData?.data.name}
+          userRole={userRole}
+          members={workspaceData?.data.members}
+          membersCount={workspaceData?.data?.members?.length}
+          setIsSidebarOpen={setIsSidebarOpen}
+          wsId={wsId}
+          setInviteModalOpen={setInviteModalOpen}
+          setCreateFormModal={setCreateFormModal}
+        />
       </div>
 
       <div className="h-[94%] p-4 flex flex-col gap-3 rounded-tl-md rounded-tr-md border bg-white shadow-md">
-        {forms.map((form, index) => (
-          <FormCard key={form.id} index={index} formId={form.id} workspaceId={id} title={form.title} status={form.status} submissions={form._count.submissions} />
-        ))}
-      </div>
-
-
-      <div className={`fixed inset-0 transition-opacity ${isSidebarOpen ? "visible opacity-100" : "invisible opacity-0 pointer-events-none"} md:hidden`}>
-
-        <div className={`absolute inset-0 bg-black/50 transition-opacity duration-300 ${isSidebarOpen ? "opacity-100" : "opacity-0" }`}onClick={() => setIsSidebarOpen(false)}/>
-
-        <div className={`absolute left-0 top-0 w-3/4 h-full bg-white shadow-xl transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
-          <div className="p-4 border-b flex justify-between items-center">
-            <h2 className="text-lg font-semibold">Workspace Menu</h2>
-            <button
-              onClick={() => setIsSidebarOpen(false)}
-              className="p-2 hover:bg-gray-100 rounded-lg"
-            >
-              <PanelLeft className="h-5 w-5" />
-            </button>
+        {workspaceData?.data.forms ? (
+          workspaceData.data.forms?.map((form, index) => (
+            <FormCard
+              key={form.id}
+              index={index}
+              formId={form.id}
+              workspaceId={workspaceData.data.id}
+              title={form.title}
+              status={form.status}
+              submissions={form._count.submissions}
+            />
+          ))
+        ) : (
+          <div className="w-full h-full flex justify-center items-center">
+            No Forms in this workspace.
           </div>
-
-          <div className="p-4"> </div>
-        </div>
+        )}
       </div>
+
+      <WorkspaceSidebarMobile
+        setIsSidebarOpen={setIsSidebarOpen}
+        isSidebarOpen={isSidebarOpen}
+      />
+
+      {isInviteModalOpen && (
+        <WorkSpaceInviteModal
+          wsId={wsId}
+          workspaceName={workspaceData?.data.name}
+          isInviteModalOpen={isInviteModalOpen}
+          setInviteModalOpen={setInviteModalOpen}
+        />
+      )}
+
+      {isCreateFormModal && (
+        <WorkspaceCreateForm
+          wsId={wsId}
+          workspaceName={workspaceData?.data.name}
+          isCreateFormModal={isCreateFormModal}
+          setCreateFormModal={setCreateFormModal}
+        />
+      )}
     </div>
   );
 };
