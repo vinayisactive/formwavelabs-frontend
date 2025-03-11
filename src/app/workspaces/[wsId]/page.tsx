74de22acb1a369e44d6585276ce1c0b5360d0133
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-03-11 23:39:24 +0530

    feat: add workspaces route with dynamic workspaceId (wsId)

--- src/app/workspaces/[wsId]/page.tsx
+++ src/app/workspaces/[wsId]/page.tsx
@@ -1,24 +1,25 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Workspace from "@/components/ui/workspace/workspace";
-import { getServerSession } from "next-auth";
+// import { getServerSession } from "next-auth";
 
 const workspace = async({params} : { params: Promise<{wsId: string}>}) => {
     const wsId = (await params).wsId; 
 
-    const session = await getServerSession(authOptions); 
-    const token = session?.accessToken; 
+    // const session = await getServerSession(authOptions); 
+    // const token = session?.accessToken; 
 
-    const data = await fetch(`https://formwavelabs-backend.alfreed-ashwry.workers.dev/api/v1/workspaces/${wsId}`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    }); 
+    // const data = await fetch(`https://formwavelabs-backend.alfreed-ashwry.workers.dev/api/v1/workspaces/${wsId}`, {
+    //     headers: {
+    //         Authorization: `Bearer ${token}`
+    //     }
+    // }); 
 
-    const workspace = await data.json(); 
+    // const workspace = await data.json(); 
+    // console.log(workspace);
 
     return(
     <div className='w-full h-full flex justify-center items-center'>
-        <Workspace workspace={workspace}/>
+        <Workspace wsId={wsId}/>
     </div>
     )
 }
