af6aab08909c7ce78ee92e1ef79ade1829d7b4a8
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-03-16 00:47:46 +0530

    refactor: remove unwanted code and comments.

--- src/app/workspaces/[wsId]/layout.tsx
+++ src/app/workspaces/[wsId]/layout.tsx
@@ -1,27 +1,11 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import WorkspaceSidebar from "@/components/ui/workspace/workspace-sidebar";
-import { getServerSession } from "next-auth";
 import React, { ReactNode } from "react";
 
 const WorkspaceLayout = async ({ children }: { children: ReactNode }) => {
-  const session = await getServerSession(authOptions);
-  const token = session?.accessToken;
-
-  const data = await fetch(
-    "https://formwavelabs-backend.alfreed-ashwry.workers.dev/api/v1/workspaces",
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-  );
-
-  const workspaces = await data.json();
-
   return (
     <div className="w-screen h-full border flex justify-center items-center bg-white">
       <div className="hidden w-[20%] md:flex border h-full">
-        <WorkspaceSidebar workspaces={workspaces.data} />
+        <WorkspaceSidebar/>
       </div>
 
       <div className="w-[100%] md:w-[80%] h-full">{children}</div>
--- src/app/workspaces/[wsId]/page.tsx
+++ src/app/workspaces/[wsId]/page.tsx
@@ -1,22 +1,7 @@
-// import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Workspace from "@/components/ui/workspace/workspace";
-// import { getServerSession } from "next-auth";
 
 const workspace = async({params} : { params: Promise<{wsId: string}>}) => {
     const wsId = (await params).wsId; 
-
-    // const session = await getServerSession(authOptions); 
-    // const token = session?.accessToken; 
-
-    // const data = await fetch(`https://formwavelabs-backend.alfreed-ashwry.workers.dev/api/v1/workspaces/${wsId}`, {
-    //     headers: {
-    //         Authorization: `Bearer ${token}`
-    //     }
-    // }); 
-
-    // const workspace = await data.json(); 
-    // console.log(workspace);
-
     return(
     <div className='w-full h-full flex justify-center items-center'>
         <Workspace wsId={wsId}/>
