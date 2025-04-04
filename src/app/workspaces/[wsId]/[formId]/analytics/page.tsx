896941de0c5a0e14a4bdd82fed027e39a88a1ba1
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-04-05 05:20:02 +0530

    feat: add analytics route - form.

---
+++ src/app/workspaces/[wsId]/[formId]/analytics/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+        <p>Analytics</p>
+    </div>
+  )
+}; 
+
+export default page
