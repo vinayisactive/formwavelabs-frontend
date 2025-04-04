4002901bdda9888a17ed58ee26e01c44cf949075
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-04-05 05:17:08 +0530

    chor: add cloudinary.com as remote path (next.config).

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         hostname: "ik.imagekit.io",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+      },
     ],
   },
 };
