f61b9cdc932cc9ea4e3f5e3539ff46074e7551aa
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-04-06 06:25:17 +0530

    feat: add analytics route (sub-route) under form.

--- src/app/workspaces/[wsId]/[formId]/analytics/page.tsx
+++ src/app/workspaces/[wsId]/[formId]/analytics/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import Analytics from '@/components/section/analytics';
 
-const page = () => {
+const page = async({params}: { params: Promise<{formId: string, wsId: string}>}) => {
+  const { formId, wsId } = (await params)
+
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-        <p>Analytics</p>
+    <div className="w-screen h-screen absolute top-0 left-0 justify-start items-start bg-white">
+      <Analytics formId={formId} workspaceId={wsId} />
     </div>
   )
 }; 
