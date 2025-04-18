d7e11cce7bd228c06bc9589bbb6d70a6a5543eb1
Vinay chaudhary <vinay.heyworld@gmail.com>
2025-04-19 01:09:42 +0530

    fix: checkbox extra-attributes required field issue fix.

--- src/components/ui/form-elements/checkbox-field/checkbox-prop-attributes.ts
+++ src/components/ui/form-elements/checkbox-field/checkbox-prop-attributes.ts
@@ -3,6 +3,7 @@ import { FormElemetInstance } from "@/utility/ts-types";
 export const checkBoxExtraAttributes = {
     label : "Checkbox label",
     helperText: "helper text...",
+    required: false
 }; 
 
 export type CheckboxCustomInstance = FormElemetInstance & {
